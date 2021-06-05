e3a93bc221e71f0bbe9743fbdff4a9b60cb75e14
Sumit Chalotra <kumar.sumit9981@gmail.com>
2021-06-06 01:39:42 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  React.useEffect(()=>{alert('Hi from sumits react app')})
+  return (
+    <div className="App" >
+        My name is Sumit Kumarrrr
+        <img alt={'sdsdsdsds'} className="App-logo" src={logo} />
+    </div>
+  );
+}
+
+export default App;
