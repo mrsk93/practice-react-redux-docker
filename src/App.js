3ab898fbdc09ed29234a8ea9b6316cdc66444119
Sumit Chalotra <kumar.sumit9981@gmail.com>
2021-06-07 02:34:17 +0530

    added and checked docker support

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  React.useEffect(()=>{alert('Hi from sumits react app')})
+  // React.useEffect(()=>{alert('Hi from sumits react app')})
   return (
     <div className="App" >
-        My name is Sumit Kumarrrr
+        My name is Sumit Kumar
         <img alt={'sdsdsdsds'} className="App-logo" src={logo} />
     </div>
   );
